f84804dd46f46377c84eef509f466e41edc74b46
Jair124 <botelloalan332@gmail.com>
2023-01-17 14:49:42 -0600

    acabe

--- js/app.js
+++ js/app.js
@@ -22,13 +22,13 @@ const productData = [
 ]
 
 const nextBtn = document.querySelector('.next-btn')
-let smImgContainer = document.querySelector('.sm-product.img-container')
+let smImgContainer = document.querySelector('.sm-product-img-container')
 let productIndex = document.querySelector('.product-index')
 let smProductDes = document.querySelector('.sm-product-des')
 let productImgCotainer = document.querySelector('.product-img-container')
 let productImg = document.querySelector('.product-img')
-let backdropimg = document.querySelector('backdrop-img')
-let productDetail = document.querySelector('.product-detail')
+let backdropImg = document.querySelector('backdrop-img')
+let productDetail = document.querySelector('.detail-product')
 let productName = document.querySelector('.product-name')
 let productDes = document.querySelector('.product-des')
 //console.log('Boton', nextBtn)
@@ -46,6 +46,24 @@ nextBtn.addEventListener('click', () =>{
     smProductDes.innerHTML = productData[currentProduct].des.slice(0, 80)
     smImgContainer.classList.add('slide')
     productImgCotainer.classList.add('slide')
-    backdropimg.classList.add('fade')
+    backdropImg.classList.add('fade')
     productDetail.classList.add('fade')
+
+    setTimeout(()=>
+    {
+        //Se espera 500 milisegundos y despues se ejecuta este codigo
+        productName.innerHTML = productData[currentProduct].name
+        productDes.innerHTML = productData[currentProduct].des
+        smImg.src = productImg.src = backdropImg.src = `img/${productData[currentProduct].image}`
+    }, 500)
+
+    setTimeout(()=>{
+        //quitamos las clases de la animacion
+        smImgContainer.classList.remove('slide')
+        productImgCotainer.classList.remove('slide')
+        backdropImg.classList.remove('fade')
+        productDetail.classList.remove('fade')
+    
+    }, 1000)
+
 })
